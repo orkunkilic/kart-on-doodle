d6c0a55ae532dcabc95dd50d27724ea064026922
Alpay Sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2020-12-22 20:53:18 +0300

    tm-link-update

--- tm/js/utils.js
+++ tm/js/utils.js
@@ -2,14 +2,13 @@ const tm_link = "https://teachablemachine.withgoogle.com/models/";
 const loc_key = location.search.split('key=')[1];
 
 function getURLParameter() {
-    if (typeof (loc_key) !== 'undefined' && loc_key.length > 3) {
+    if (typeof(loc_key) !== 'undefined' && loc_key.length > 3) {
         if (loc_key.includes(tm_link))
             return loc_key;
         else
             return tm_link + loc_key;
     } else {
-        //return tm_link + 'jPLYI-Dlf/';
-        return tm_link + "bSWNuNwZa/";
+        return tm_link + 'jPLYI-Dlf/';
+        //return tm_link + "bSWNuNwZa/";
     }
 }
-
